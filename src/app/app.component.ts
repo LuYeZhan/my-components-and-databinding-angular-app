3e769182e9d57ad9576f755d0fbbc6396e4f30cb
Lu Ye Zhan <lu.ye.external@axa.com>
2020-02-03 14:49:44 +0100

    seeing lifecycle hooks in action

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from "@angular/core";
 })
 export class AppComponent {
   serverElements = [
-    { type: "server", name: "Testsever", content: "Just a test!" }
+    { type: "server", name: "Testserver", content: "Just a test!" }
   ];
 
   onServerAdded(serverData: { serverName: string; serverContent: string }) {
@@ -27,5 +27,13 @@ export class AppComponent {
       name: blueprintData.serverName,
       content: blueprintData.serverContent
     });
+  }
+
+  onChangeFirst() {
+    this.serverElements[0].name = "Changed!";
+  }
+
+  onDestroyFirst() {
+    this.serverElements.splice(0, 1);
   }
 }
--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,4 +1,17 @@
-import { Component, OnInit, Input, ViewEncapsulation } from "@angular/core";
+import {
+  Component,
+  OnInit,
+  Input,
+  ViewEncapsulation,
+  OnChanges,
+  SimpleChanges,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked,
+  OnDestroy
+} from "@angular/core";
 
 @Component({
   selector: "app-server-element",
@@ -6,10 +19,53 @@ import { Component, OnInit, Input, ViewEncapsulation } from "@angular/core";
   styleUrls: ["./server-element.component.css"],
   encapsulation: ViewEncapsulation.Emulated // None, Native can be used too. default is ViewEncapsulation.Emulated, which uses the shadow DOM
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent
+  implements
+    OnInit,
+    OnChanges,
+    DoCheck,
+    AfterContentInit,
+    AfterContentChecked,
+    AfterViewInit,
+    AfterViewChecked,
+    OnDestroy {
   @Input("srvElement") element: { type: string; name: string; content: string };
+  @Input() name: string;
 
-  constructor() {}
+  constructor() {
+    console.log("constructor Called!");
+  }
 
-  ngOnInit() {}
+  ngOnChanges(changes: SimpleChanges) {
+    console.log("ngOnChanges is Called!");
+    console.log(changes);
+  }
+
+  ngOnInit() {
+    console.log("ngOnInit is Called!");
+  }
+
+  ngDoCheck() {
+    console.log("ngDoCheck is Called!");
+  }
+
+  ngAfterContentInit() {
+    console.log("ngAfterContentInit is Called!");
+  }
+
+  ngAfterContentChecked() {
+    console.log("ngAfterContentChecked is Called!");
+  }
+
+  ngAfterViewInit() {
+    console.log("ngAfterViewInit is Called!");
+  }
+
+  ngAfterViewChecked() {
+    console.log("ngAfterViewChecked is Called!");
+  }
+
+  ngOnDestroy() {
+    console.log("ngOnDestroy is Called!");
+  }
 }
