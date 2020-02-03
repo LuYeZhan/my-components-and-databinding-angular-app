8d6e54741d32f4591980c33bec4c6d887783c904
Lu Ye Zhan <lu.ye.external@axa.com>
2020-02-03 09:59:14 +0100

    binding to custom events, between cockpit, app-server, and app-component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,25 @@ import { Component } from "@angular/core";
 })
 export class AppComponent {
   serverElements = [
-    { type: "server", name: "Testsever", content: "Just a test" }
+    { type: "server", name: "Testsever", content: "Just a test!" }
   ];
+
+  onServerAdded(serverData: { serverName: string; serverContent: string }) {
+    this.serverElements.push({
+      type: "server",
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
+
+  onBlueprintAdded(blueprintData: {
+    serverName: string;
+    serverContent: string;
+  }) {
+    this.serverElements.push({
+      type: "blueprint",
+      name: blueprintData.serverName,
+      content: blueprintData.serverContent
+    });
+  }
 }
