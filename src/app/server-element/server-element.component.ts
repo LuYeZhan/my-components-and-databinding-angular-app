c5e656ad0d0504d21053185af6d22efb9391a940
Lu Ye Zhan <lu.ye.external@axa.com>
2020-02-03 11:18:27 +0100

    using local references in templates

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -4,7 +4,7 @@ import { Component, OnInit, Input, ViewEncapsulation } from "@angular/core";
   selector: "app-server-element",
   templateUrl: "./server-element.component.html",
   styleUrls: ["./server-element.component.css"],
-  encapsulation: ViewEncapsulation.Emulated // None, Native can be used too
+  encapsulation: ViewEncapsulation.Emulated // None, Native can be used too. default is ViewEncapsulation.Emulated, which uses the shadow DOM
 })
 export class ServerElementComponent implements OnInit {
   @Input("srvElement") element: { type: string; name: string; content: string };
